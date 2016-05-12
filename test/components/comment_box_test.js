01ca869c572fb4a928fef4f6e9eda4547c222959
Qasim Laghari <go.jets.laghari@gmail.com>
2016-05-12 18:00:36 -0500

    Update comment box test

---
+++ test/components/comment_box_test.js
@@ -0,0 +1,15 @@
+import {renderComponent, expect} from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+
+describe('Comment Box', () => {
+    it('has a text area', () => {
+        const component = renderComponent(CommentBox);
+        expect(component.find('textarea')).to.exist;
+    });
+
+    it('has a button', () => {
+        const component = renderComponent(CommentBox);
+        expect(component.find('button')).to.exist;
+    });
+});
