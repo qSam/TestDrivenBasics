c74b9913c99472f27c5eff9d2176950b6add35aa
Qasim Laghari <go.jets.laghari@gmail.com>
2016-05-15 21:47:45 -0500

    Create comment list test and component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -15,4 +15,9 @@ describe('App', () => {
       expect(component.find('.comment-box')).to.exist;
   });
 
+
+  it('shows a comment list', () => {
+        expect(component.find('.comment-list')).to.exist;
+    });
+
 });
