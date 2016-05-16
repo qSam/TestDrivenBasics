9c85c90c0cdfc8eabc388b8a8e7fcca7e04bef39
Qasim Laghari <go.jets.laghari@gmail.com>
2016-05-15 21:37:12 -0500

    Add Chai Test for Form Submission

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,13 +4,15 @@ import App from '../../src/components/app';
 
 //Use describe to group together similar tests
 describe('App', () => {
-  //Use 'it' to test a single attribute of a target
-  it('Shows the correct text', ()=>{
-      //Create an instance of App
-      const component = renderComponent(App);
 
-      //Use 'expect' to make an assertion about a target
-      expect(component).to.contain('React simple starter');
+  let component;
+
+  beforeEach( ()=> {
+    component = renderComponent(App);
+  });
+
+  it('shows a comment box', () => {
+      expect(component.find('.comment-box')).to.exist;
   });
 
 });
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -22,4 +22,21 @@ describe('Comment Box Test', () => {
     it('has a button', () => {
         expect(component.find('button')).to.exist;
     });
+
+    describe('entering some text', ()=> {
+
+          beforeEach( ()=> {
+              component.find('textarea').simulate('change','new comment');
+          });
+
+          it('shows text in the text area', ()=> {
+            expect(component.find('textarea')).to.have.value('new comment');
+          });
+
+          it('when submitted, clears the input', ()=> {
+            component.simulate('submit');
+            expect(component.find('textarea')).to.have.value('');
+          });
+    });
+
 });
